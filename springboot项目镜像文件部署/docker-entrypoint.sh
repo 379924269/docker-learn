e6deafca0663899596d5db78535a6cef914c5ce0
huazai <379924269@qq.com>
2025-03-17 08:55:25 +0800

    docker 本地docker部署最小jar

---
+++ springboot项目镜像文件部署/docker-entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+java $JAVA_OPTS -server \
+-XX:+UnlockExperimentalVMOptions \
+-XX:+UseCGroupMemoryLimitForHeap \
+-XX:-OmitStackTraceInFastThrow \
+-Djava.security.egd=file:/dev/./urandom \
+org.springframework.boot.loader.JarLauncher
